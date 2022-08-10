398784d7e42a6165df4eab6b10b9955359e3c4ef
YongZhen Low <cephyz@gmail.com>
2022-08-11 06:16:33 +0800

    feat(auth): update login form

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,57 +1,63 @@
 import { useState } from 'react'
-import { supabase } from '../utils/supabase-client'
+import { supabase } from 'utils/supabase-client'
+import type { ChangeEvent, FormEvent } from 'react'
 
-export default function Auth() {
-  const [loading, setLoading] = useState(false)
-  const [email, setEmail] = useState('')
+const Auth = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [email, setEmail] = useState<string>('')
 
-  const handleLogin = async (email: string) => {
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    // @todo handle validation here
+    if (isLoading || !email?.length) {
+      return false
+    }
+
     try {
-      setLoading(true)
+      setIsLoading(true)
       const { error } = await supabase.auth.signIn({ email })
       if (error) throw error
       alert('Check your email for the login link!')
     } catch (error: any) {
       alert(error.error_description || error.message)
     } finally {
-      setLoading(false)
+      setIsLoading(false)
     }
   }
 
   return (
-    <div className="flex flex-row justify-center text-slate-700 text-center mt-10">
-      <form
-        action=""
-        method=""
-        className=""
-        onSubmit={(e) => {
-          e.preventDefault()
-          handleLogin(email)
-        }}
-      >
-        <h1 className="text-2xl my-4">Chat App</h1>
+    <div className="container max-w-md mx-auto mt-10 px-6 text-slate-900 text-center">
+      <div className="rounded border border-slate-200 p-10">
+        <h1 className="font-medium text-2xl mb-6">Login to Chat</h1>
 
-        <p className="my-3">Sign in via magic link with your email below</p>
+        <form onSubmit={handleLogin} className="space-y-6">
+          <label className="block">
+            <span>
+              Get a magic link sent to your email that&rsquo;ll sign you in
+              instantly
+            </span>
+            <input
+              className="block mt-3 w-full rounded border text-slate-900 border-slate-400 py-3 px-3"
+              type="email"
+              placeholder="Your email"
+              value={email}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setEmail(e.target.value)
+              }
+            />
+          </label>
 
-        <div className="my-3">
-          <input
-            className="form-input w-full rounded border text-slate-700 border-slate-400 py-3 px-3"
-            type="email"
-            placeholder="Your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-
-        <div className="my-3">
           <button
-            className="block w-full rounded py-3 px-5 bg-teal-600 hover:bg-teal-700 text-white"
-            disabled={loading}
+            className="block w-full rounded py-3 px-5 text-white bg-blue-800 hover:bg-blue-700 disabled:bg-slate-400"
+            disabled={isLoading}
           >
-            <span>{loading ? 'Loading...' : 'Send magic link'}</span>
+            {isLoading ? 'Loading...' : 'Send Magic Link'}
           </button>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   )
 }
+
+export default Auth
